fdf9e412384223999c42e9b7abbf755030edbec5
梁子哥哥 <444693341@qq.com>
2020-04-15 11:13:16 +0800

    第一个项目simpe

--- vuePlugin/VueMathPlugin.js
+++ vuePlugin/VueMathPlugin.js
@@ -3,13 +3,13 @@
 export default{
     //在调用install 需要调用Vue
     install:function(Vue){
-    //自定义指令   square  用的话指令需要是v-square
-    // Vue.directive('square', function (el, binding) {
-    //     el.innerHTML = Math.pow(binding.value,2)
-    //     });
-    // Vue.directive('sqrt',function(el,binding){
-    //     el.innerHTML = Math.sqrt(binding.value)
-    // });
+    // 自定义指令   square  用的话指令需要是v-square
+    Vue.directive('square', function (el, binding) {
+        el.innerHTML = Math.pow(binding.value,2)
+        });
+    Vue.directive('sqrt',function(el,binding){
+        el.innerHTML = Math.sqrt(binding.value)
+    });
     Vue.directive('sin',function(el,binding){
         el.innerHTML = Math.sin(Math.PI * binding.value / 180)
     });
--- vuePlugin/script.js
+++ vuePlugin/script.js
@@ -1,12 +1,25 @@
 
 import Vue from 'vue/dist/vue.js'
 import VueMathPlugin from './VueMathPlugin.js'
+import Vuex from 'vuex'
+
 
 Vue.use(VueMathPlugin)
+Vue.use(Vuex)
+
+var store = new Vuex.store({
+    state:{message:'hello'},
+    mutations:{},
+    actions:{},
+    getters:{},
+})
+
+
 
 new Vue({
     el:'#app',
     data:{
         item:90
-    }
+    },
+    store:store
 })
